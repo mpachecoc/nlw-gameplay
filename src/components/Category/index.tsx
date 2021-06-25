8995d7cf01d2e9745da84e7f3657841f2ba50dff
Mauricio Pacheco <mauriciopachecoc@gmail.com>
2021-06-24 22:27:23 -0400

    Complete Home UI screen

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -28,12 +28,12 @@ export function Category({
         colors={[secondary30, secondary50]}
       >
         <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-          <View style={checked ? styles.checked : styles.unchecked}>
-            <Icon 
-              width={48} 
-              height={48} 
-            />
-          </View>
+          <View style={checked ? styles.checked : styles.unchecked} />
+            
+          <Icon 
+            width={48} 
+            height={48} 
+          />
 
           <Text style={styles.title}>
             {title}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,49 @@
-import React from 'react'
-import { View, Text, Image } from 'react-native'
+import React, { useState } from 'react'
+import { View, FlatList } from 'react-native'
 
 import { Profile } from '../../components/Profile'
 import { ButtonAdd } from '../../components/ButtonAdd'
 import { CategorySelect } from '../../components/CategorySelect'
+import { ListHeader } from '../../components/ListHeader'
+import { Appointment } from '../../components/Appointment'
+import { ListDivider } from '../../components/ListDivider'
 
 import { styles } from './styles'
 
 export function Home() {
+  const [category, setCategory] = useState('')
+  
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 at 20:40',
+      description: 'It\'s today that we are going to arrive without losing a battle.'
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 at 20:40',
+      description: 'It\'s today that we are going to arrive without losing a battle.'
+    }
+  ]
+
+  function handleCategorySelected(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId)
+  }
+
   return (
     <View>
       <View style={styles.header}>
@@ -15,9 +51,29 @@ export function Home() {
         <ButtonAdd />
       </View>
 
-      <View>
-        <CategorySelect />
+      <CategorySelect 
+        categorySelected={category} 
+        setCategory={handleCategorySelected}
+      />
+
+      <View style={styles.content}>
+        <ListHeader 
+          title="Scheduled Matches"
+          subtitle="Total 6"
+        />
+
+        <FlatList 
+          data={appointments}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Appointment data={item} />
+          )}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
+      
     </View>
   )
 }
